62046bc4bc3280fe2607e50b00911e06cc075594
Patricia Robles <patriciarobles.tech@gmail.com>
2023-03-13 12:30:39 +0000

    initial setup

---
+++ Models/Animal.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+
+namespace ZooManagement.Models
+{
+    public class Animal
+    {
+        public int Id {get; set;}
+        public string Type { get; set; }
+
+        public string Name
+
+       
+	}
+}
+
